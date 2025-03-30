948877bea45560b8cabfd8b10f87f606836e3680
gkouretas <thegreekgeorge07@gmail.com>
2025-03-29 18:21:26 -0700

    Changes/commits toward real-time fatigue inference

---
+++ add_python_utils_symlink.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+TARGET=$1
+PATH="$(pwd)/src/$TARGET/$TARGET"
+PYTHON_UTILS_PATH="$(pwd)/python_utils"
+
+# Check if both arguments are provided
+if [ -z "$TARGET" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+# Check if target exists
+if [ ! -e "$PATH" -o ! -e "$PYTHON_UTILS_PATH" ]; then
+    echo "Error: Target '$PATH' and/or '$PYTHON_UTILS_PATH' does not exist."
+    exit 1
+fi
+
+if [ -L "$PATH/python_utils" ]; then
+    echo "Symlink already exists, exiting."
+    exit 0
+fi
+
+/usr/bin/ln -s "$PYTHON_UTILS_PATH" "$PATH/python_utils"
+
+if [ -L "$PATH/python_utils" ]; then
+    echo "Symlink created, exiting."
+    exit 0
+else
+    echo "Failed to create symlink."
+    exit 0
+fi
